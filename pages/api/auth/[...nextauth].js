bed7ace1555c8fdb83c3a9afe62584d2ac8758eb
maximoortelli <maximoortelli@hotmail.com>
2024-04-06 15:16:08 -0300

    /login all ready.
    Everything is ready, the database is connected and everything works correctly.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import GithubProvider from "next-auth/providers/github"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
 
 export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -13,5 +16,20 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  pages: {
+     signIn: '/login',
+  },
+  session: {
+    strategy: 'jwt',
+    jwt: true,
+  },
+  callbacks: {
+    session: async ({token, session}) => {
+      if(session?.user && token?.sub) {
+        session.user.id = token.sub;
+      }
+      return session;
+    }
+  }
 }
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
--- pages/login.js
+++ pages/login.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {getProviders, signIn} from "next-auth/react";
+import {getProviders, signIn, useSession} from "next-auth/react";
 
 export default function LoginPage({providers}) {
-
-  
-
+  const {data, status} = useSession();
+  console.log({data, status});
   return (
     <div className='flex items-center justify-center h-screen'>
          {Object.values(providers).map(provider => (
             <div key={provider.name}> 
                 <button 
                      onClick={async () => {await signIn(provider.id)}}
-                     className='bg-twitterWhite pl-3 pr-5  px-5 py-2 text-black 
-                     rounded-full flex items-center'>
+                     className='bg-twitterWhite pl-3 m-3 pr-5 gap-3 px-5 py-2 text-black 
+                     rounded-full flex items-center justify-between'>
 
                     <img src='/google.png' alt='' className='h-8' />
                      Sign in with {provider.name}
